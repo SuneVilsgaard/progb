0a94e52537fe858da61d9a9cc8fed0cc6639601b
julemanden123 <179506522+iff07@users.noreply.github.com>
2026-04-26 19:00:29 +0200

    glemte lige en af siderne til brugen men nu ser alle godt ud

--- ProgB/Properties/Resources.Designer.cs
+++ ProgB/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace ProgB.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap football {
+            get {
+                object obj = ResourceManager.GetObject("football", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap hal_3 {
             get {
                 object obj = ResourceManager.GetObject("hal 3", resourceCulture);
